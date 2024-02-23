e3c42c39e1c79c84bc9e4712bc38186b67aac4aa
karimmohsen841 <160882879+karimmohsen841@users.noreply.github.com>
2024-02-23 13:15:17 +0100

    version 23/02 matin

--- main.js
+++ main.js
@@ -25,3 +25,25 @@ $closeBtnsArr.forEach(function($btn) {
   });
 });
 
+
+
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
